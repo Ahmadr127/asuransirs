92b7b934f038fbd8149a75e0b9ffca890d6298f4
Ahmadr127 <ahmdrifai808@gmail.com>
2026-09-24 11:52:19 +0700

    cdn layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Bridging Asuransi')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @include('layouts.partials.tailwind-cdn')
 
     <link rel="icon" type="image/x-icon" href="{{ asset('images/logo.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
